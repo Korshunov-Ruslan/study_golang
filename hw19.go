1f8d3cc3f946709545beed19bf01aa2bb6c794dc
Korshunov-Ruslan <thenyky1937@gmail.com>
2022-03-04 16:17:58 +0300

    fixed old hw

--- hw19.go
+++ hw19.go
@@ -8,7 +8,8 @@ import (
 func getNewArray(firArr []int, secArr []int) []int {
 	sort.Ints(firArr)
 	sort.Ints(secArr)
-	var newestArr []int
+	lenTwoArr := len(firArr) + len(secArr)
+	newestArr := make([]int, 0, lenTwoArr)
 	n, m := len(firArr), len(secArr)
 	i, j := 0, 0
 	newestArr = append(newestArr)
@@ -22,9 +23,12 @@ func getNewArray(firArr []int, secArr []int) []int {
 		}
 	}
 	if len(firArr) < len(secArr) {
-		copy(newestArr[j+i-1:], secArr[j:])
+		newestArr = append(newestArr, secArr[j:]...)
 	} else if len(firArr) > len(secArr) {
-		copy(newestArr[j+i-1:], firArr[i:])
+		newestArr = append(newestArr, secArr[i:]...)
+	} else {
+		newestArr = append(newestArr, secArr[i:]...)
+		newestArr = append(newestArr, secArr[j:]...)
 	}
 	return newestArr
 }
@@ -32,8 +36,8 @@ func main() {
 	/*
 		Function that merges 2 sorted arrays.
 	*/
-	firstArray := []int{99, 12, 33, 1}
-	secondArray := []int{1, 91, 2, 3, 100}
+	firstArray := []int{2, 12, 33, 33}
+	secondArray := []int{1, 91, 2, 99}
 	fmt.Println(getNewArray(firstArray, secondArray))
 }
 
