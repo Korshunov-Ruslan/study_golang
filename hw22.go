bac4dd3e1f952262d65ca08398621f0935640e31
Korshunov-Ruslan <thenyky1937@gmail.com>
2022-03-09 21:57:14 +0300

    new hw

---
+++ hw22.go
@@ -0,0 +1,63 @@
+package main
+
+import "fmt"
+
+//import (
+//	"fmt"
+//	"math/rand"
+//	"time"
+//)
+//
+//const elem = 12
+//
+//func main() {
+//	/*
+//		Populate an array with unordered numbers based on a random number generator. Enter the number.
+//		The program must find this number in the array and output how many numbers are in the array after the entered one. If there is no entered number in the array, output 0.
+//		For the convenience of checking, implement displaying the array on the screen.
+//	*/
+//	var value int
+//	fmt.Println("Enter a number what you want to find")
+//	fmt.Scan(&value)
+//	fmt.Println(findNumber(arrayGenerator(), value))
+//}
+//
+//func arrayGenerator() [elem]int {
+//	rand.Seed(time.Now().UnixNano())
+//	var array [elem]int
+//	for i := 0; i < elem; i++ {
+//		array[i] = rand.Intn(elem * 12)
+//	}
+//	fmt.Println(array)
+//	return array
+//}
+//func findNumber(arr [elem]int, value int) (result int) {
+//	for i := 0; i < elem; i++ {
+//		if arr[i] == value {
+//			result = len(arr[i:]) - 1
+//		}
+//	}
+//	return
+//}
+
+func main() {
+	/*
+		Fill in an ordered array of 12 elements and enter a number.
+		It is necessary to implement the search for the first occurrence of a given number in an array.
+		The complexity of the algorithm should be minimal.
+	*/
+	array := [12]int{1, 2, 2, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	neededValue := 2
+	fmt.Println(getIndexOfNumber(array, neededValue))
+}
+func getIndexOfNumber(arr [12]int, value int) int {
+	index := -1
+	for i := 0; i < 12; i++ {
+		fmt.Println(i)
+		if arr[i] == value {
+			index = i
+			break
+		}
+	}
+	return index
+}
--- test.go
+++ test.go
@@ -1,12 +1,41 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
 
-func acceptFunc(a, b, c int, F func(int, int) int) int {
-	return a + F(b, c)
-}
+const el = 10
+
 func main() {
-	fmt.Println(acceptFunc(10, 20, 30, func(b int, c int) int {
-		return b + c
-	}))
+	rand.Seed(time.Now().UnixNano())
+	var arr [el]int
+	for i := 0; i < 10; i++ {
+		arr[i] = 10*i + rand.Intn(10)
+	}
+	fmt.Println(arr)
+	value := arr[rand.Intn(el)]
+	fmt.Println(value)
+	fmt.Println(binarySearch(arr, value))
+	value = rand.Intn(500)
+	fmt.Println(value)
+	fmt.Println(binarySearch(arr, value))
+}
+func binarySearch(arr [el]int, value int) int {
+	index := -1
+	min := 0
+	max := el - 1
+	for max >= min {
+		middle := (max + min) / 2
+		if arr[middle] == value {
+			index = middle
+			break
+		} else if arr[middle] > value {
+			max = middle - 1
+		} else {
+			min = middle + 1
+		}
+	}
+	return index
 }
