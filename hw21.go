e5f82c2ed0c2ee6d3320c359df9ae94e4b7a55d2
Korshunov-Ruslan <thenyky1937@gmail.com>
2022-03-09 16:17:23 +0300

    fixed old hw

--- hw21.go
+++ hw21.go
@@ -2,29 +2,29 @@ package main
 
 import "fmt"
 
-//func main() {
-//	/*
-//		S = 2 × x + y ^ 2 − 3/z;  x = int16, y = uint8 ; z = float32.
-//	*/
-//	a := func(x int16, y uint8, z float32) float32 {
-//		return 2*float32(x) + float32(y*y) - 3/z
-//	}
-//	fmt.Println(a(10, 1, 2))
-//}
-
 func main() {
 	/*
-		Write a function that takes a function of the form A func (int, int) int as input,
-		and wraps it inside and calls it on exit.
+		S = 2 × x + y ^ 2 − 3/z;  x = int16, y = uint8 ; z = float32.
 	*/
-	f := func(a int, b int) int { return a * b }
-	ff := func(a int, b int) int { return a + b }
-	fff := func(a int, b int) int { return a - b }
-	iAcceptfunc(1, 2, f)
-	iAcceptfunc(1, 2, ff)
-	iAcceptfunc(1, 2, fff)
+	a := func(x int16, y uint8, z float32) float32 {
+		return 2*float32(int64(x)) + float32(int64(y)*int64(y)) - 3/float32(z)
+	}
+	fmt.Println(a(10, 17, 2))
 }
-func iAcceptfunc(a, b int, f func(int, int) int) {
-	defer fmt.Println(f(1, 2))
 
-}
+//func main() {
+//	/*
+//		Write a function that takes a function of the form A func (int, int) int as input,
+//		and wraps it inside and calls it on exit.
+//	*/
+//	f := func(a int, b int) int { return a * b }
+//	ff := func(a int, b int) int { return a + b }
+//	fff := func(a int, b int) int { return a - b }
+//	iAcceptfunc(1, 2, f)
+//	iAcceptfunc(1, 2, ff)
+//	iAcceptfunc(1, 2, fff)
+//}
+//func iAcceptfunc(a, b int, f func(int, int) int) {
+//	defer fmt.Println(f(1, 2))
+//
+//}
